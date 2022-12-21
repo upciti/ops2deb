66e8e7be6d4653c842b2f92d47188f20b817e2d2
Simon Guigui <simon.guigui@parkoview.com>
2022-12-21 20:46:40 +0100

    fix: cwd template variable not working as expected

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,10 @@
+import os
+from pathlib import Path
+
 import pytest
 
 from ops2deb.exceptions import Ops2debError
-from ops2deb.utils import separate_results_from_errors
+from ops2deb.utils import separate_results_from_errors, working_directory
 
 
 def test_separate_results_from_errors_should_separate_results_from_ops2deb_exceptions():
@@ -18,3 +21,19 @@ def test_separate_results_from_errors_should_raise_when_exception_is_not_an_ops2
     test = {0: error}
     with pytest.raises(RuntimeError):
         separate_results_from_errors(test)
+
+
+def test_working_directory__should_set_current_working_directory_within_context(tmp_path):
+    origin = Path().absolute()
+    with working_directory(tmp_path):
+        assert Path(os.getcwd()) != origin
+        assert Path(os.getcwd()) == tmp_path
+
+
+def test_working_directory__should_restore_current_directory_when_context_is_left(
+    tmp_path,
+):
+    origin = Path().absolute()
+    with working_directory(tmp_path):
+        pass
+    assert Path(os.getcwd()) == origin
